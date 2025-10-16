43a4247eb8b4cdc25c1c7aeb3ea82d52242dcd9a
Kim Morris <kmorris3@sandi.net>
2025-10-15 21:27:41 -0700

    styles
    fixed stylesheet liking

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // script.js
 // Currently empty, but you can add interactivity here later.
 // Example: Smooth scrolling for navigation links.
-//document.querySelectorAll('nav a').forEach(anchor => {
-//    anchor.addEventListener('click', function(e) {
-//        e.preventDefault();
-//        document.querySelector(this.getAttribute('href')).scrollIntoView({
-//            behavior: 'smooth'
-//        });
-//    });
-//});
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
